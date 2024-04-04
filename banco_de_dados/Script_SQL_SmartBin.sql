82d65feb037ae3156259855682acc125e1690156
MaikonDsGomes <160528424+MaikonDsGomes@users.noreply.github.com>
2024-04-04 20:34:14 -0300

    update BD

--- banco_de_dados/Script_SQL_SmartBin.sql
+++ banco_de_dados/Script_SQL_SmartBin.sql
@@ -16,21 +16,27 @@ insert into Empresa values
 
 create table Lixeira (
 IdLixeira int primary key auto_increment,
-Localização varchar (50),
+Logradouro varchar (50),
+Bairro varchar(50),
+DtTime datetime,
 Nivel varchar (50));
 desc lixeira;
 
-insert into Lixeira values
-(default, 'Vila Nova', 'Cheia' );
+insert into Lixeira (Logradouro, Bairro, DtTime, Nivel) 
+values 
+('Rua Principal', 'Centro', '2024-04-04 08:30:00', 'Baixo'),
+('Avenida dos Pássaros', 'Parque das Árvores', '2024-04-04 09:45:00', 'Médio'),
+('Rua das Flores', 'Jardim Botânico', '2024-04-04 10:20:00', 'Alto');
 
 select * from Lixeira;
 
 create table Usuário (
 IdUsuario int primary key auto_increment,
-TipoUsuario varchar (50) constraint chkTipo check (TipoUsuario in ('Administrador', 'Comum')),
+TipoUsuario varchar (50),
 NomeUsuario varchar (50),
 EmailUsuario varchar (50),
-Senha varchar (15)
+Senha varchar (15),
+constraint chkTipo check (TipoUsuario in ('Administrador', 'Comum'))
 );
 desc Usuário;
 
