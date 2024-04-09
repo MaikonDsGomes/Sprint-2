b2e768f54c94ed907b0b3aa9d5bb8c6adb67f3bb
Ayrton Casa <ayrton.casa@sptech.school>
2024-04-09 20:36:05 -0300

    BDfeito_CadastroMelhorado

--- banco_de_dados/Script_SQL_SmartBin.sql
+++ banco_de_dados/Script_SQL_SmartBin.sql
@@ -3,60 +3,70 @@ use SmartBin;
 
 create table Empresa (
 Cnpj char (14) primary key,
-email varchar(60),
-	constraint chkemail check (email like '%@%' and email like '%.com'),
-Senha varchar (50),
+NomeEmpresa varchar(45),
 RazaoSocial varchar (50),
 QtdLixeiras int);
 desc Empresa;
 
 insert into Empresa values
- (012345678965412, 'smartbin@gmail.com', 'Binlixeira321', 'Sustentavel', 3);
+ (012345678965412, 'Smartbin','Sustentavel', 3);
  
  select * from Empresa;
+
+create table Usuario (
+cpf char(11) primary key,
+TipoUsuario varchar (50),
+constraint chkTipo check (TipoUsuario in ('Administrador', 'Comum')),
+NomeUsuario varchar (50),
+EmailUsuario varchar (50),
+	constraint chEmailUsuario check (EmailUsuario like '%@%' and EmailUsuario like '%.com'),
+Senha varchar (15),
+fkEmpresa char(14), 
+	constraint fkUsuarioEmpresa foreign key (fkEmpresa)
+    references empresa(Cnpj)
+);
+desc Usuario;
+
+Insert into Usuario values
+(12345678901, 'Administrador', 'Ricardo', 'ricardo@gmail.com', 'Alegria54321', 012345678965412);
+
+select * from Usuario;
 
 create table Lixeira (
 IdLixeira int primary key auto_increment,
 Logradouro varchar (50),
 Bairro varchar(50),
-DtTime datetime,
-Metade bit(1),
-Cheia bit(1),
+Complemento varchar(45),
 fkEmpresa char(14),
 	constraint fkLixeiraEmpresa foreign key (fkEmpresa)
     references Empresa(Cnpj)
 );
 desc lixeira;
 
-insert into Lixeira (Logradouro, Bairro, DtTime, Metade, Cheia, fkEmpresa) 
-values 
-('Rua Principal', 'Centro', '2024-04-04 08:30:00', 1, 0,012345678965412),
-('Avenida dos Pássaros', 'Parque das Árvores', '2024-04-04 09:45:00', 0, 0, 012345678965412),
-('Rua das Flores', 'Jardim Botânico', '2024-04-04 10:20:00', 1, 1, 012345678965412);
+insert into Lixeira (Logradouro,Bairro,Complemento,fkEmpresa) values 
+('Rua Principal', 'Centro', 'Ao lado do MacDonalds', 012345678965412),
+('Avenida dos Pássaros', 'Parque das Árvores', 'Em frente ao Bar do Zé', 012345678965412),
+('Rua das Flores', 'Jardim Botânico', 'Ao lado da Faculdade SPTech', 012345678965412);
 
 select * from Lixeira;
 
-create table Usuario (
-cpf char(11) primary key,
-TipoUsuario varchar (50),
-NomeUsuario varchar (50),
-EmailUsuario varchar (50),
-	constraint chEmailUsuario check (EmailUsuario like '%@%' and EmailUsuario like '%.com'),
-Senha varchar (15),
-constraint chkTipo check (TipoUsuario in ('Administrador', 'Comum')),
-fkEmpresa char(14), 
-	constraint fkUsuarioEmpresa foreign key (fkEmpresa)
-    references empresa(Cnpj)
+create table historico(
+idHistorico int primary key auto_increment,
+DtTime datetime,
+Metade bit(1),
+Cheia bit(1),
+fkLixeira int,
+	constraint fkHistoricoLixeira foreign key (fkLixeira)
+    references Lixeira(idLixeira)
 );
-desc Usuário;
 
-Insert into Usuário values
-(12345678901, 'Administrador', 'Ricardo', 'ricardo@gmail.com', 'Alegria54321', 012345678965412);
+insert into historico (DtTime, Metade, Cheia, fkLixeira) values
+('2024-08-04 11:00', 1, 0, 1),
+('2024-05-24 12:00', 1, 1, 2),
+('2024-04-08 15:37', 0, 0, 3)
+;
 
-select * from Usuário;
-
-
-
-
-
-
+select historico.DtTime, historico.Metade, historico.cheia, Lixeira.Logradouro, Lixeira.Bairro, Lixeira.Complemento, Lixeira.fkEmpresa 
+	from historico join Lixeira
+    on historico.fkLixeira = Lixeira.idLixeira
+    join empresa on lixeira.fkEmpresa = empresa.Cnpj;
