bae0aceed57d463986fabdfc5e84b142cdea1148
kevellyoliveira <kevelly.oliveira@sptech.school>
2024-06-04 13:29:57 -0300

    alterando...

--- site_institucional/src/models/usuarioModel.js
+++ site_institucional/src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT idUsuario, NomeUsuario, EmailUsuario, fkEmpresa as empresaId, TipoUsuario, telefone, Senha FROM usuario WHERE EmailUsuario = '${email}' AND Senha = '${senha}';
+        SELECT idUsuario, NomeUsuario, EmailUsuario, fkEmpresa as empresaId, TipoUsuario, telefone, Senha FROM Usuario WHERE EmailUsuario = '${email}' AND Senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
